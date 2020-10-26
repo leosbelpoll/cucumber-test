e1d57dd28d9921db3a59cb702117ecabc45c9325
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-10-26 13:43:06 -0300

    Add better interactive cucumber report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"seleniumgluecode"})
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = {"seleniumgluecode"},
+        plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports.cucumber-report.html"}
+)
 public class TestRunner {
 
 }
