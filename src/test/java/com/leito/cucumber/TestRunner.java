e7a4e3ffe68019adfad427d95a420f217c3121ba
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-10-26 12:16:08 -0300

    Add json report

--- src/test/java/com/leito/cucumber/TestRunner.java
+++ src/test/java/com/leito/cucumber/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:login/LoginTest.feature", glue = "com.leito.cucumber", plugin = {"html:target/cucumber-test-report", "junit:target/junit-xml-report.xml"})
+@CucumberOptions(features = "classpath:login/LoginTest.feature", glue = "com.leito.cucumber", plugin = {"html:target/cucumber-test-report", "junit:target/junit-xml-report.xml", "json:target/json-report.json"})
 public class TestRunner {
 
 }
